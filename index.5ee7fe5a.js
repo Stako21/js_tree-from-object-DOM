de575384c67093686c6264a86de7793e6ee689d4
Stanislav Kohut <stako211@gmail.com>
2024-10-13 16:40:42 +0300

    make build

---
+++ index.5ee7fe5a.js
@@ -0,0 +1,2 @@
+var e=document.createElement("div");e.id="tree",document.body.appendChild(e),function e(n,t){var r=document.createElement("ul");for(var a in t){var d=document.createElement("li");d.textContent=a,Object.keys(t[a]).length>0&&e(d,t[a]),r.appendChild(d)}n.appendChild(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.5ee7fe5a.js.map
